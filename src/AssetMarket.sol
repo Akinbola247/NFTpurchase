9587465e5bd564dbbd9a35260f465e8c5137aa0e
akinbola247 <akinbolakehinde247@gmail.com>
2023-03-23 17:20:41 +0100

    first commit

---
+++ src/AssetMarket.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "lib/chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "lib/openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
+contract Market {
+     using SafeCast for int256;
+struct ItemDetails{
+    uint tokenID;
+    uint seller;
+}
+
+address owner;
+IERC721 nftAddress;
+IERC20 DaiContract;
+AggregatorV3Interface internal ETHusdpriceFeed;
+AggregatorV3Interface internal DAIusdpriceFeed;
+mapping(uint => ItemDetails) itemInfo;
+
+
+constructor(address _nftAddress){
+    nftAddress = IERC721(_nftAddress);
+    DaiContract= IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    owner = msg.sender;
+
+    DAIusdpriceFeed = AggregatorV3Interface(0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9);
+    ETHusdpriceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+
+}
+function listItem(uint _tokenitemID) public {
+    ItemDetails storage _a = temInfo[_tokenitemID];
+    _a.tokenID = _tokenitemID;
+    _a.seller = msg.sender;
+}
+function purchaseItem(uint _tokenID) public payable{
+    uint balance = DaiContract.balanceOf(msg.sender);
+    require(balance >= 3.5 ether);
+    uint ethusdCurrentPrice = getETHUSDPrice();
+    uint usdtusdCurrentPrice = getDAIUSDPrice();
+    uint _amountINUSdt = (3.5 ether * ethusdCurrentPrice)/usdtusdCurrentPrice;
+    DaiContract.transferFrom(msg.sender, address(this), _amountINUSdt);
+    nftAddress.transferFrom(owner, msg.sender, _tokenID);
+ }
+
+
+function getDAIUSDPrice() public view returns (uint) {
+        ( , int price, , , ) = DAIusdpriceFeed.latestRoundData();
+        return price.toUint256();
+    }
+function getETHUSDPrice() public view returns (uint) {
+        ( , int price, , , ) = ETHusdpriceFeed.latestRoundData();
+        return price.toUint256();
+    }
+
+}
+
+
---
+++ test/Counter.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "lib/forge-std/src/Test.sol";
+import "../src/AssetMarket.sol";
+import "../src/KZN.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract AssetMarket is Test {
+    Market public market;
+    NFT public nft;
+    IERC20 public daiContract;
+function setUp() public {
+    vm.startPrank(0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb);
+    uint mainnet = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/xypdsCZYrlk6oNi93UmpUzKE9kmxHy2n", 16890887);
+    vm.selectFork(mainnet);
+    nft = new NFT();
+    market = new Market(address(nft));
+    daiContract = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    vm.stopPrank();
+}
+
+function testMint() public {
+    vm.startPrank(0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb);
+    nft.safeMint("https://ipfs.filebase.io/ipfs/QmYqEcCNJiP7pP2nzSsvyv7Ji1tNpv6omWMJ4Nph22dmfn", address(market));
+    vm.stopPrank();
+}
+function testPurchaseItem() public {
+    testMint(); 
+    vm.startPrank(0x748dE14197922c4Ae258c7939C7739f3ff1db573);
+    daiContract.approve(address(market), 35000000000000000000000000000000000000000000000000000);
+    market.purchaseItem(0);
+    uint balance = daiContract.balanceOf(address(market));
+}
+
+}
